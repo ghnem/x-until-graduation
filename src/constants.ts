e0dfe6138e4ab9f6f861e8673459e094bd478b86
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-22 03:53:29 +0200

    Updates

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-const MILLISECONDS_IN_WEEK = MILLISECONDS_IN_DAY  * 7;
-
-const firstDay = new Date(2017, 8, 24, 0, 0).getTime();
-const lastDay = new Date(2022, 6, 31, 0, 0).getTime();
-
-export { firstDay, lastDay, MILLISECONDS_IN_DAY, MILLISECONDS_IN_WEEK };
