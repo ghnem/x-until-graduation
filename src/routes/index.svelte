3da6220c07a214c7fcbe8bff930188a83f89f392
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-22 03:52:06 +0200

    Fix page padding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,34 +4,31 @@
   import PorgressBar from "../components/Progress.svelte";
   import Weeks from "../components/Weeks.svelte";
 
-  export const title = 'X Until Graduation'
-  export const description = 'The number of days, and weeks until the 2022 class of CSE Minya University graduate.'
-  export const altDescription = 'Graduation hat on top of books'
-  export const url = 'https://demos.abdulrhmnghanem.tech/x-until-graduation/'
-  export const img = '/x-until-graduation/education.png'
+  export const title = "X Until Graduation";
+  export const description =
+    "The number of days, and weeks until the 2022 class of CSE Minya University graduate.";
+  export const altDescription = "Graduation hat on top of books";
+  export const url = "https://demos.abdulrhmnghanem.tech/x-until-graduation/";
+  export const img = "/x-until-graduation/education.png";
 </script>
 
-
 <head>
-    <title>{title}</title>
-    <meta name="description" content={description}>
-    <!-- OG -->
-    <meta property="og:title" content="{title}" />
-    <meta property="og:url" content="{url}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="{description}" />
-    <meta property="og:image" content="{img}" />
-    <meta property="og:image:alt" content="{altDescription}" />
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <!-- OG -->
+  <meta property="og:title" content={title} />
+  <meta property="og:url" content={url} />
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={img} />
+  <meta property="og:image:alt" content={altDescription} />
 
   <!-- Twitter -->
-  <meta property="twitter:card" content="{altDescription}" />
-  <meta property="twitter:url" content="{url}" />
-  <meta property="twitter:title" content="{title}" />
-  <meta property="twitter:description" content="{description}" />
-  <meta
-    property="twitter:image"
-    content="{img}"
-  />
+  <meta property="twitter:card" content={altDescription} />
+  <meta property="twitter:url" content={url} />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={img} />
   <meta
     property="twitter:image:alt"
     content="Image description for accessibility"
@@ -47,6 +44,12 @@
 <Footer />
 
 <style>
+  @media (min-width: 800px) {
+    h1,
+    p {
+      padding-left: 2rem;
+    }
+  }
   main {
     margin: auto;
     width: 1000px;
@@ -56,7 +59,7 @@
   p {
     font-size: 18px;
   }
-  @media(max-width: 800px) {
+  @media (max-width: 800px) {
     main {
       width: 300px;
     }
